4ed3eb5afb78b13783549b90c42601c53e66e355
beejsbj <burooj.bj@gmail.com>
2022-12-17 16:41:47 -0500

    changed api for digimon

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import monsters from "./digidex.json" assert { type: "json" };
+import * as prismicH from '@prismicio/helpers';
+import { client } from './config/prismicConfig.js';
+
 
 const app = express();
 const PORT = 1986;
@@ -7,25 +10,41 @@ app.listen(PORT, function () {
 	console.log("listening to port");
 });
 
+
 // app.view("view engine", "ejs"); WRONG
 app.set("view engine", "ejs");
 app.use(express.static("styles"));
 app.use(express.static("images"));
 
+// Add a middleware function that runs on every route. It will inject 
+// the prismic context to the locals so that we can access these in 
+// our templates.
+app.use((request, response, next) => {
+  response.locals.get = prismicH;
+  next()
+})
 
-app.get("/", function (request, response) {
-	// response.render('partials/menu'); doesnt work
-	response.render("pages/home");
+
+
+
+
+app.get("/", async function (request, response) {
+	const homeData = await client.getByUID('page', 'home')
+	const data = homeData.data;
+	response.render("pages/home", { data });
 });
 
-app.get("/monsters", function (request, response) {
+// digimon api
+app.get("/monsters", async function (request, response) {
+	const monstersData = await fetch(`https://www.digi-api.com/api/v1/digimon?pageSize=20`)
+	let monsters = await monstersData.json();
+	monsters = monsters.content;
 	response.render("pages/monsters-list/monsters-list", { monsters });
 });
 
-app.get("/monsters/:name", function (request, response) {
-	let monster = monsters.find(function (monster) {
-		return monster.name.toLowerCase() == request.params.name.toLowerCase();
-	});
+app.get("/monsters/:name", async function (request, response) {
+	const monsterData = await fetch(`https://www.digi-api.com/api/v1/digimon/${request.params.name}`)
+	const monster = await monsterData.json();
 	response.render("pages/monster-detail/monster-detail", { monsters, monster });
 });
 
@@ -48,8 +67,3 @@ app.use(function (request, response) {
 	response.status(404).render("pages/404", { query: request.url });
 });
 
-function findDigimon(id) {
-	return monsters.find(function (monster) {
-		return monster.id == id || monster.name.toLowerCase() == id;
-	});
-}
